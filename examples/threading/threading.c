3e4219743870bd220f3115637f55d34fc3c5bea0
Octavio <octavio.chiman@gm.com>
2024-02-16 15:33:31 -0500

    Assignment 4 part-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,7 +13,41 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    //printf("New thread started, PID %d TID %d\n", getpid(), (pid_t)syscall(SYS_gettid));
+    DEBUG_LOG("New thread started");
+    //thread_func_args->thread_id = syscall(SYS_gettid);
+    // wait
+    if (thread_func_args == NULL)
+    {
+        ERROR_LOG ("Malloc error");
+        return thread_param;
+    }
+
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    
+    // obtain mutex
+    if ( pthread_mutex_lock(thread_func_args->mutex) != 0 ) {
+         ERROR_LOG ("pthread_mutex_lock failed");
+         thread_func_args->thread_complete_success = false;
+         return thread_param;
+    }
+
+      // wait to release
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+
+    // release mutex  
+    if ( pthread_mutex_unlock(thread_func_args->mutex) != 0 ) {
+            ERROR_LOG ("pthread_mutex_unlock failed");
+            thread_func_args->thread_complete_success = false;
+            return thread_param;
+    }
+
+    DEBUG_LOG("pthread_mutex unlock successfull");
+    thread_func_args->thread_complete_success = true;
+
+
     return thread_param;
 }
 
@@ -28,6 +62,35 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    struct thread_data* data_shared = (struct thread_data*) malloc(sizeof(struct thread_data));
+    int rc = 0;
+ 
+    // malloc to pass data to the thread
+    DEBUG_LOG("Main thread, PID TID", getpid(), (pid_t)syscall(SYS_gettid));
+
+    if (data_shared == NULL)
+    {
+        ERROR_LOG("Malloc error");
+        return false;
+    }
+
+    data_shared->wait_to_obtain_ms = wait_to_obtain_ms;
+    data_shared->wait_to_release_ms = wait_to_release_ms;
+    data_shared->mutex = mutex;
+
+    DEBUG_LOG("Mutex successfully initialized");
+
+    // start the thread         
+    rc = pthread_create(thread, NULL, threadfunc, (void *) data_shared);
+    if (rc != 0)
+    {
+        ERROR_LOG("Error creating thread");
+        return false;
+    }
+    DEBUG_LOG("Thread created");
+
+    //return the status of threat creation, true or false
+    return true;
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -19,6 +19,15 @@ struct thread_data{
      * Set to true if the thread completed with success, false
      * if an error occurred.
      */
+    /**
+    * The mutex used to lock this account when manipulating values, for thread safety
+    */ 
+    pthread_mutex_t *mutex;
+    
+    int wait_to_obtain_ms;
+    
+    int wait_to_release_ms;
+
     bool thread_complete_success;
 };
 
